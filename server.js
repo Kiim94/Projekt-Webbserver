d9f46e061a0c2df1c1fe33e0c70d5ddfa4aa2f98
Kiim94 <nemomoa@hotmail.com>
2026-05-26 14:19:55 +0200

    Lagt till cors

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ const PORT = process.env.PORT || 3000;
 
 
 //middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://bonan.netlify.app"
+    ],
+    credentials:true
+}));
 
 app.use(express.json());
 
